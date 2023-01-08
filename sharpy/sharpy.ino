e4bff7be2ea303ec47043b1cf6eb87a4a89e6c4b
Dávid Molnár <mdavid626@gmail.com>
2023-01-08 10:29:59 +0100

    Update sharpy.ino

--- sharpy/sharpy.ino
+++ sharpy/sharpy.ino
@@ -1,6 +1,14 @@
+// Digital Inputs
 #define BTN_START 2
 #define BTN_STOP 3
 
+// Analog Inputs
+#define POT_F1 0
+#define POT_P1 1
+#define POT_F2 2
+#define POT_P2 3
+
+// Digital Outputs
 #define RELAY1 4
 #define RELAY2 5
 #define LED_MAIN 6
@@ -12,14 +20,11 @@
 #define LED_F2_3 12
 #define LED_P2 13
 
-#define POT_F1 0
-#define POT_P1 1
-#define POT_F2 2
-#define POT_P2 3
-
+// Delays
 #define POT_MAX_MS 10000
-#define BASE_DELAY 50
+#define BASE_DELAY_MS 50
 
+// Phases
 #define PHASE_F1 0
 #define PHASE_P1 1
 #define PHASE_F2_1 2
@@ -31,26 +36,29 @@
 
 bool isOn = true;
 int currentDelay = 0;
-int phase = PHASE_F1;
+int currentPhase = PHASE_F1;
 
-int getDurationInMs(int pinNumber) {
+int getDelayInMs(int pinNumber) {
   int value = analogRead(pinNumber);
   return POT_MAX_MS * (double)value / 1023;
 }
 
-int getDelayByPhase() {
-  switch (phase) {
-    case PHASE_F1: return getDurationInMs(POT_F1);
-    case PHASE_P1: return  getDurationInMs(POT_P1);
+int getDelayInMsByPhase() {
+  switch (currentPhase) {
+    case PHASE_F1:
+      return getDelayInMs(POT_F1);
+    case PHASE_P1:
+      return  getDelayInMs(POT_P1);
     case PHASE_F2_1: 
     case PHASE_F2_2:
     case PHASE_F2_3:
-      return getDurationInMs(POT_F2);
+      return getDelayInMs(POT_F2);
     case PHASE_P2_1:
     case PHASE_P2_2:
     case PHASE_P2_3:
-      return getDurationInMs(POT_P2);
-    default: return 0;
+      return getDelayInMs(POT_P2);
+    default:
+      return 0;
   }
 }
 
@@ -76,9 +84,9 @@ void setup() {
 void loop() {
   if (digitalRead(BTN_START) == HIGH) {
     isOn = true;
-    digitalWrite(LED_RUN, HIGH);
     currentDelay = 0;
-    phase = PHASE_F1;
+    currentPhase = PHASE_F1;
+    digitalWrite(LED_RUN, HIGH);
     digitalWrite(LED_F1, HIGH);
     digitalWrite(RELAY1, HIGH);
   }
@@ -96,65 +104,65 @@ void loop() {
   }
 
   if (isOn) {
-    int delay = getDelayByPhase();
+    int delay = getDelayInMsByPhase();
     if (currentDelay >= delay) {
       currentDelay = 0;
-      switch (phase) {
+      switch (currentPhase) {
         case PHASE_F1:
-          phase = PHASE_P1;
+          currentPhase = PHASE_P1;
           digitalWrite(LED_F1, LOW);
           digitalWrite(RELAY1, LOW);
           digitalWrite(LED_P1, HIGH);
           break;
         case PHASE_P1:
-          phase = PHASE_F2_1;
+          currentPhase = PHASE_F2_1;
           digitalWrite(LED_P1, LOW);
           digitalWrite(LED_F2_1, HIGH);
           digitalWrite(RELAY2, HIGH);
           break;
         case PHASE_F2_1:
-          phase = PHASE_P2_1;
+          currentPhase = PHASE_P2_1;
           digitalWrite(LED_F2_1, LOW);
           digitalWrite(LED_P2, HIGH);
           digitalWrite(RELAY2, LOW);
           break;
         case PHASE_P2_1:
-          phase = PHASE_F2_2;
+          currentPhase = PHASE_F2_2;
           digitalWrite(LED_P2, LOW);
           digitalWrite(LED_F2_2, HIGH);
           digitalWrite(RELAY2, HIGH);
           break;
         case PHASE_F2_2:
-          phase = PHASE_P2_2;
+          currentPhase = PHASE_P2_2;
           digitalWrite(LED_F2_2, LOW);
           digitalWrite(LED_P2, HIGH);
           digitalWrite(RELAY2, LOW);
           break;
         case PHASE_P2_2:
-          phase = PHASE_F2_3;
+          currentPhase = PHASE_F2_3;
           digitalWrite(LED_P2, LOW);
           digitalWrite(LED_F2_3, HIGH);
           digitalWrite(RELAY2, HIGH);
           break;
         case PHASE_F2_3:
-          phase = PHASE_P2_3;
+          currentPhase = PHASE_P2_3;
           digitalWrite(LED_F2_3, LOW);
           digitalWrite(LED_P2, HIGH);
           digitalWrite(RELAY2, LOW);
           break;
         case PHASE_P2_3:
-          phase = PHASE_F1;
+          currentPhase = PHASE_F1;
           digitalWrite(LED_P2, LOW);
           digitalWrite(LED_F1, HIGH);
           digitalWrite(RELAY1, HIGH);
           break;
       }
     }
-    Serial.print('f');
-    Serial.println(delay);
-    Serial.print('c');
+    Serial.print("phase: ");
+    Serial.println(currentPhase);
+    Serial.print("delay: ");
     Serial.println(currentDelay);
-    currentDelay += BASE_DELAY;
+    currentDelay += BASE_DELAY_MS;
   }
-  delay(BASE_DELAY);
+  delay(BASE_DELAY_MS);
 }
